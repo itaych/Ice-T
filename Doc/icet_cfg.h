itaych/Ice-T

commit 6dece9313fcea9c8df2dd4cc32a2deccb13bca7d

2020-05-15 - snapshot

src/vt2.asm
@@ -7,7 +7,7 @@
 ; This part	is resident in bank #1
 
 	.bank
-	*=	$4010
+	*=	$4000
 
 ; VT-100 TERMINAL EMULATOR
 
@@ -23,7 +23,8 @@ connect
 	lda #0
 	sta x
 	sta y
-	jsr mkblkchr
+	lda #BLOCK_CHARACTER
+	sta prchar
 	jsr print		; draw block if XOFF is on
 ?ok1
 	jsr do_term_main_display
@@ -85,15 +86,14 @@ do_term_main_display
 	pla
 	sta bckgrnd
 	jsr boldon
-	lda boldallw
-	cmp #3
+	cpy #3		; y contains value of boldallw, 3 means blink
 	bne ?nbl	; If blink - turn blinking characters on by disabling PMs
 	lda #0
-	ldx #3
+	; y happens to contain 3 which is exactly what's needed for this loop
 ?bf
-	sta hposp0,x
-	sta hposm0,x
-	dex
+	sta hposp0,y
+	sta hposm0,y
+	dey
 	bpl ?bf
 	lda sdmctl
 	and #~11110011	; Disable PM DMA
@@ -492,16 +492,13 @@ vt100_check_hi_chars
 	jmp vt100_done_check_hi_chars
 
 putcrs			; Cursor flasher
-	lda ty
-	tay
-	dey
-	asl a
-	tax
-	lda linadr,x
+	ldx ty
+	lda linadr_l,x
 	sta cntrl
-	lda linadr+1,x
+	lda linadr_h,x
 	sta cntrh
-	lda lnsizdat,y	; 0 or 4 and up - small, 1-3 - big
+	dex
+	lda lnsizdat,x	; 0 or 4 and up - small, 1-3 - big
 	beq smcurs
 	cmp #4
 	bcc bigcurs
@@ -1114,12 +1111,10 @@ hash_process
 	sta y
 ?lpy
 	inc y
-	lda y
-	asl a
-	tax
-	lda linadr,x
+	ldx y
+	lda linadr_l,x
 	sta cntrl
-	lda linadr+1,x
+	lda linadr_h,x
 	sta cntrh
 	ldx #0
 ?lpx
@@ -2434,8 +2429,7 @@ ersfmcurs
 	ldy tx
 	sty x
 	ldx ty
-	dex
-	lda lnsizdat,x	; wide line?
+	lda lnsizdat-1,x	; wide line?
 	beq ?ok
 	tya 		; yes, multiply X position by 2, taking care not to go beyond edge
 	asl a
@@ -2489,12 +2483,10 @@ txerfm
 	bne erfmbt
 	rts
 erfmbt
-	lda ty			; mass erase by blanking whole bytes of screen data
-	asl a
-	tax
-	lda linadr,x	; get data address for this line
+	ldx ty			; mass erase by blanking whole bytes of screen data
+	lda linadr_l,x	; get data address for this line
 	sta cntrl
-	lda linadr+1,x
+	lda linadr_h,x
 	sta cntrh
 	lda x
 	ldx temp		; was this a wide line?
@@ -2589,12 +2581,10 @@ txerto
 	bpl ertobt
 	rts
 ertobt
-	lda ty			; mass erase by blanking whole bytes of screen data
-	asl a
-	tax
-	lda linadr,x	; get data address for this line
+	ldx ty			; mass erase by blanking whole bytes of screen data
+	lda linadr_l,x	; get data address for this line
 	sta cntrl
-	lda linadr+1,x
+	lda linadr_h,x
 	sta cntrh
 	lda x
 	ldx temp		; was this a wide line?
@@ -2666,17 +2656,14 @@ printerm
 ; 2 - x2 width, double height, upper half
 ; 3 - x2 width, double height, lower half
 
-	lda y
-	tay
-	asl a
-	tax
-	lda txlinadr-2,x
+	ldy y
+	lda txlinadr_l-1,y
 	sta ersl
-	lda txlinadr-1,x
+	lda txlinadr_h-1,y
 	sta ersl+1
-	lda linadr,x
+	lda linadr_l,y
 	sta cntrl
-	lda linadr+1,x
+	lda linadr_h,y
 	sta cntrh
 	ldx prchar
 	lda invsbl
@@ -2810,10 +2797,10 @@ ignrsh
 	jmp prt1
 nopcchar
 	; for characters < 128 we have a lookup table
-	lda chrtbll,x
+	lda chrtbl_l,x
 	sta prchar
 	sta plc2+1
-	lda chrtblh,x
+	lda chrtbl_h,x
 	sta prchar+1
 	sta plc2+2
 prt1
@@ -3185,7 +3172,6 @@ boldbok
 	bcs ?s4
 	sta boldsct,x
 ?s4
-	lda y
 	cmp boldscb,x
 	bcc ?sk
 	sta boldscb,x
@@ -3251,7 +3237,7 @@ bolduok
 ; terminal screen. So, backscroll buffer is not updated, line size table is not changed, text mirror is not scrolled.
 
 scrldown
-	lda scrltop	; Move scrolled-out line into screen-saver
+	lda scrltop	; Move scrolled-out line into backscroll memory
 	cmp #1
 	bne noscrsv	; (but only if top of scroll region is top line)
 	lda outnum
@@ -3262,9 +3248,9 @@ scrldown
 	beq ?ok
 	dec looklim
 ?ok
-	lda txlinadr
+	lda txlinadr_l
 	sta ersl
-	lda txlinadr+1
+	lda txlinadr_h
 	sta ersl+1
 	ldy #0
 	jsr scrllnsv
@@ -3290,43 +3276,37 @@ noscrsv
 	sta lnsizdat-1,x
 nodolnsz
 	lda scrlbot
-	tax
-	asl scrlbot
-	cpx scrltop
+	cmp scrltop
 	beq scdnadbd
-	lda scrltop	; Scroll address-table
-	asl a
-	tax
-	lda linadr,x
+	ldx scrltop	; Scroll address-table
+	lda linadr_l,x
 	sta nextlnt
-	lda linadr+1,x
+	lda linadr_h,x
 	sta nextlnt+1
 scdnadlp
-	lda linadr+2,x
-	sta linadr,x
-	lda linadr+3,x
-	sta linadr+1,x
-	inx
+	lda linadr_l+1,x
+	sta linadr_l,x
+	lda linadr_h+1,x
+	sta linadr_h,x
 	inx
 	cpx scrlbot
 	bne scdnadlp
 	jmp scdnadok
 scdnadbd
 	ldx scrlbot ;	If top=bot, no scroll occurs
-	lda linadr,x
+	lda linadr_l,x
 	sta nextlnt
-	lda linadr+1,x
+	lda linadr_h,x
 	sta nextlnt+1
 scdnadok
 	lda nextln
-	sta linadr,x
+	sta linadr_l,x
 	lda nextln+1
-	sta linadr+1,x
+	sta linadr_h,x
 	lda nextlnt
 	sta nextln
 	lda nextlnt+1
 	sta nextln+1
-	lsr scrlbot
 
 	lda outnum
 	cmp #255
@@ -3336,37 +3316,29 @@ scdnadok
 	sbc scrltop
 	beq dncltxln
 	tax
-	lda scrltop
-	asl a
-	tay
+	ldy scrltop
 	dey
-	dey
-	lda txlinadr,y
+	lda txlinadr_l,y
 	pha
-	lda txlinadr+1,y
+	lda txlinadr_h,y
 	pha
 dntbtxlp
-	lda txlinadr+2,y
-	sta txlinadr,y
-	lda txlinadr+3,y
-	sta txlinadr+1,y
-	iny
+	lda txlinadr_l+1,y
+	sta txlinadr_l,y
+	lda txlinadr_h+1,y
+	sta txlinadr_h,y
 	iny
 	dex
 	bne dntbtxlp
 	pla
-	sta txlinadr+1,y
+	sta txlinadr_h,y
 	pla
-	sta txlinadr,y
+	sta txlinadr_l,y
 dncltxln
-	lda scrlbot
-	asl a
-	tax
-	dex
-	dex
-	lda txlinadr,x
+	ldx scrlbot
+	lda txlinadr_l-1,x
 	sta ersl
-	lda txlinadr+1,x
+	lda txlinadr_h-1,x
 	sta ersl+1
 	ldy #79
 	lda #32
@@ -3540,85 +3512,67 @@ scrlup			; SCROLL UP
 	lda scrlbot ;	Scroll line-adr tbl
 	cmp scrltop
 	beq ?ab
-	asl scrltop
-	lda scrlbot
-	asl a
-	tax
-	lda linadr,x
+	ldx scrlbot
+	lda linadr_l,x
 	sta nextlnt
-	lda linadr+1,x
+	lda linadr_h,x
 	sta nextlnt+1
 ?al
-	lda linadr-1,x
-	sta linadr+1,x
-	lda linadr-2,x
-	sta linadr,x
-	dex
+	lda linadr_l-1,x
+	sta linadr_l,x
+	lda linadr_h-1,x
+	sta linadr_h,x
 	dex
 	cpx scrltop
 	bne ?al
 	beq ?ak
 ?ab
-	lda scrltop
-	asl a
-	sta scrltop
-	tax
-	lda linadr,x
+	ldx scrltop
+	lda linadr_l,x
 	sta nextlnt
-	lda linadr+1,x
+	lda linadr_h,x
 	sta nextlnt+1
 ?ak
 	lda nextln
-	sta linadr,x
+	sta linadr_l,x
 	lda nextln+1
-	sta linadr+1,x
+	sta linadr_h,x
 	lda nextlnt
 	sta nextln
 	lda nextlnt+1
 	sta nextln+1
 
-	lda scrltop
-	lsr a
-	sta scrltop ;	Scroll text mirror
+	lda scrltop ;	Scroll text mirror
 	cmp scrlbot
 	beq ?et
 	sec
 	lda scrlbot
-	pha
+	tax
 	sbc scrltop
 	tay
-	pla
-	asl a
-	tax
 	dex
-	dex
-	lda txlinadr,x
+	lda txlinadr_l,x
 	sta ersl
-	lda txlinadr+1,x
+	lda txlinadr_h,x
 	sta ersl+1
 ?tl
-	lda txlinadr-2,x
-	sta txlinadr,x
-	lda txlinadr-1,x
-	sta txlinadr+1,x
-	dex
+	lda txlinadr_l-1,x
+	sta txlinadr_l,x
+	lda txlinadr_h-1,x
+	sta txlinadr_h,x
 	dex
 	dey
 	bne ?tl
 	lda ersl
-	sta txlinadr,x
+	sta txlinadr_l,x
 	lda ersl+1
-	sta txlinadr+1,x
+	sta txlinadr_h,x
 	jmp ?gu
 ?et
-	lda scrltop
-	asl a
-	tax
-	dex
-	dex
-	lda txlinadr,x
+	ldx scrltop
+	lda txlinadr_l-1,x
 	sta ersl
-	lda txlinadr+1,x
+	lda txlinadr_h-1,x
 	sta ersl+1
 ?gu
 	ldy #0
@@ -3935,16 +3889,13 @@ prep_boldface_scroll
 
 ; erase line Y (1-24)
 ersline
-	lda y
-	beq ersline_done
+	lda y				; Don't erase text mirror if y=0.
+	beq ersline_done	; Note that Y coordinate is expected in A
 	; erase text mirror
-	asl a
 	tax
-	dex
-	dex
-	lda txlinadr,x
+	lda txlinadr_l-1,x
 	sta cntrl
-	lda txlinadr+1,x
+	lda txlinadr_h-1,x
 	sta cntrh
 	ldy #79
 	lda #32
@@ -4017,24 +3968,22 @@ lookup			; Buffer-scroll UP
 novrup
 	jsr crsifneed
 
-	lda linadr+48	; Scroll linadr table
+	lda linadr_l+24	; Scroll line address table
 	pha
-	lda linadr+49
+	lda linadr_h+24
 	pha
-	ldx #46
+	ldx #23
 lkupscadlp
-	lda linadr,x
-	sta linadr+2,x
-	lda linadr+1,x
-	sta linadr+3,x
+	lda linadr_l,x
+	sta linadr_l+1,x
+	lda linadr_h,x
+	sta linadr_h+1,x
 	dex
-	dex
-	cpx #0
 	bne lkupscadlp
 	lda nextln
-	sta linadr+2
+	sta linadr_l+1
 	lda nextln+1
-	sta linadr+3
+	sta linadr_h+1
 	pla
 	sta nextln+1
 	pla
@@ -4079,14 +4028,13 @@ lkupwtvb ; continue fine scroll
 	rts
 lkupcrs
 	jsr vdelayr	; Coarse-scroll
-	ldx #2
+	ldx #1
 	ldy #10
 lkupsclp
-	lda linadr,x
+	lda linadr_l,x
 	sta dlist+4,y
-	lda linadr+1,x
+	lda linadr_h,x
 	sta dlist+5,y
-	inx
 	inx
 	tya
 	clc
@@ -4128,24 +4076,23 @@ lookdn			; Buffer-scroll DOWN
 novrdn
 	jsr crsifneed
 
-	lda linadr+2	; Scroll linadr table
+	lda linadr_l+1	; Scroll line address table
 	pha
-	lda linadr+3
+	lda linadr_h+1
 	pha
-	ldx #2
+	ldx #1
 lkdnscadlp
-	lda linadr+2,x
-	sta linadr,x
-	lda linadr+3,x
-	sta linadr+1,x
+	lda linadr_l+1,x
+	sta linadr_l,x
+	lda linadr_h+1,x
+	sta linadr_h,x
 	inx
-	inx
-	cpx #48
+	cpx #24
 	bne lkdnscadlp
 	lda nextln
-	sta linadr,x
+	sta linadr_l,x
 	lda nextln+1
-	sta linadr+1,x
+	sta linadr_h,x
 	pla
 	sta nextln+1
 	pla
@@ -4204,13 +4151,10 @@ lkzro
 	jmp lkdnprdn
 
 lkoky
-	asl a
 	tax
-	dex
-	dex
-	lda txlinadr,x
+	lda txlinadr_l-1,x
 	sta lookln2
-	lda txlinadr+1,x
+	lda txlinadr_h-1,x
 	sta lookln2+1
 	asl eitbit
 lkdnprlp
@@ -4248,14 +4192,13 @@ lkdnvbwt		; continue fine-scroll
 
 lkdndocr
 	jsr vdelayr	; Coarse scroll
-	ldx #2
+	ldx #1
 	ldy #10
 lkdnsclp
-	lda linadr,x
+	lda linadr_l,x
 	sta dlist+4,y
-	lda linadr+1,x
+	lda linadr_h,x
 	sta dlist+5,y
-	inx
 	inx
 	tya
 	clc
@@ -5112,13 +5055,12 @@ captbfdo
 	lda captold
 	beq ?skip
 	tax
-	lda #27
+	lda #BLOCK_CHARACTER
 ?lp2
 	sta captdt-1,x
 	dex
 	bne ?lp2
 ?skip
-	jsr mkblkchr
 	ldx #>captpr
 	ldy #<captpr
 	jmp prmesg
@@ -5139,22 +5081,21 @@ bufcntdo
 	bpl ?lp1
 	tya
 	sta oldbufc
+	lsr a
+	lsr a
+	lsr a
 	beq ?dtok
-	lsr a
-	lsr a
-	lsr a
 	tax
 	cpx #9
 	bcc ?notbig
 	ldx #8
 ?notbig
-	lda #27
+	lda #BLOCK_CHARACTER
 ?dtmk
 	sta bufcntdt-1,x
 	dex
 	bne ?dtmk
 ?dtok
-	jsr mkblkchr
 	ldx #>bufcntpr
 	ldy #<bufcntpr
 	jmp prmesg
@@ -5228,9 +5169,9 @@ ledsdo
 	bne ?lp
 
 	; draw character
-	lda linadr
+	lda linadr_l
 	sta cntrl
-	lda linadr+1
+	lda linadr_h
 	sta cntrh
 
 	ldy #25 ; horizontal offset of location to draw character
@@ -5256,7 +5197,7 @@ led_mask_tbl .byte 0, $f0, $0f, $ff
 
 ; End of status line handlers
 
-vdelayr			; Waits for next VBI to finish
+vdelayr			; Waits for next VBI to finish, while polling serial port
 	lda rtclock_2
 ?v
 	ldx fastr	; Checks on buffer, too.
@@ -5270,12 +5211,10 @@ vdelayr			; Waits for next VBI to finish
 	rts
 
 filline ; Fill line with 'on' pixels (value 0)
-	lda y
-	asl a
-	tax
-	lda linadr,x
+	ldx y
+	lda linadr_l,x
 	sta cntrl
-	lda linadr+1,x
+	lda linadr_h,x
 	sta cntrh
 	lda #0
 	jmp filline_custom_value
@@ -6189,12 +6128,10 @@ prmesgy
 	rts
 
 invbarmk		; Put an inverse bar
-	lda y
-	asl a
-	tax
-	lda linadr,x
+	ldx y
+	lda linadr_l,x
 	sta cntrl
-	lda linadr+1,x
+	lda linadr_h,x
 	sta cntrh
 	ldy #0
 ?lp
@@ -6613,7 +6550,7 @@ boldcolrtables_hi	.byte >colortbl_0, >colortbl_1, >colortbl_2, >colortbl_3, >col
 ; white, red, green, yellow, blue, magenta, cyan
 bold2color_xlate	.byte $0a, $0e, 70, 74, 200, 206, 234, 238, 132, 136, 88, 92, 152, 156
 
-; Bold - tables filled at program start
+; Bold - static tables filled at program start
 boldpmus	.ds 40	; convert column number to PM number (0-4)
 boldtbpl	.ds 5	; low-byte pointer to each player data
 boldtbph	.ds 5	; high
@@ -6622,8 +6559,8 @@ boldwri		.ds 8	; same as boldwr but inverted (running 0's)
 boldytb		.ds 25	; converts line number to vertical offset within PM
 
 ; Bold - Dynamic data:
-boldsct		.ds 5	; Per PM, current uppermost (lower value) bold line
-boldscb		.ds 5	; Per PM, current lowest (higher value) bold line
+boldsct		.ds 5	; Per PM, current uppermost (lowest value) bold line
+boldscb		.ds 5	; Per PM, current lowest (highest value) bold line
 boldypm		.ds 5	; Flag whether there are any enabled pixels in this PM
 
 ; Dialer's stuff:
@@ -6641,8 +6578,7 @@ mini1
 	.error "mini1>wind1!!"
 	.endif
 
-; Move all of the above crap into
-; banked memory
+; Move all of the above crap into banked memory
 	.bank
 	*=	$600
 inittrm
@@ -6658,25 +6594,12 @@ chbnk1  ldx bank1	; this value is modified to bank2 for second iteration
 	stx banksw
 	sta (cntrl),y
 	iny
-	cpy #0
 	bne intrmlp
 	inc cntrh
 	lda cntrh
 chbnk2  cmp #>mini1
 	bcc intrmlp
 	beq intrmlp
-
-	; store special string at $4000 to mark this bank as properly loaded.
-	; in subsequent loads (e.g. after reboot), if this magic marker is found then loading this bank
-	; is skipped.
-	ldx #0
-?lp
-	lda svscrlms,x
-	sta banked_memory_bottom,x
-	inx
-	cpx #$10
-	bne ?lp
-
 	lda bank0
 	sta banksw
 
@@ -6688,5 +6611,11 @@ chbnk2  cmp #>mini1
 	rts
 
 	.bank
-	*=	$2e2
+	*=	dos_initad
 	.word	inittrm
+
+;; This is just a workaround for WUDSN so labels are recognized during development. It is ignored during assembly.
+	.if 0
+	.include icet.asm
+	.endif
+;; End of WUDSN workaround

src/vt3.asm
@@ -6,9 +6,8 @@
 
 ; This part	is resident in bank #2
 
-;
 	.bank
- 	*=	$4010
+ 	*=	$4000
 
 mnmenu
 	lda mnlnofbl
@@ -1015,13 +1014,12 @@ mxstrt
 	sta x
 	iny
 	lda (prfrom),y
-	asl a
 	tax
-	lda linadr,x
 	clc
+	lda linadr_l,x
 	adc x
 	sta invlo
-	lda linadr+1,x
+	lda linadr_h,x
 	adc #0
 	sta invhi
 	lda nodoinv
@@ -1243,10 +1241,10 @@ invsub
 	ldx mnmnucnt
 	lda ?tb,x
 	clc
-	adc linadr
+	adc linadr_l
 	sta invlo
 	lda #0
-	adc linadr+1
+	adc linadr_h
 	sta invhi
 	jmp doinv
 
@@ -5659,3 +5657,9 @@ sendcans
 	dex
 	bne ?l2
 	rts
+
+;; This is just a workaround for WUDSN so labels are recognized during development. It is ignored during assembly.
+	.if 0
+	.include icet.asm
+	.endif
+;; End of WUDSN workaround

src/vtdt.asm
@@ -8,14 +8,14 @@
 
 ; All menu-related data tables
 
-; Most title screen messages here
+; Most title screen messages here. A typical message is composed of x, y, length, string.
 
 tilmesg2
 	.byte	(80-75)/2,8,75
 	.byte	"Telecommunications software for the Atari 8-bit. (c)1993-2014 Itay Chamiel."
 .if 1
 tilmesg4
-	.byte	(80-75)/2,13,71
+	.byte	(80-71)/2,13,71
 	.byte	"This software is free, but donations are always appreciated (via Paypal"
 tilmesg5
 	.byte	(80-25)/2,14,25
@@ -607,10 +607,9 @@ xferfl2
 
 attnst	.ds	32	; Zmodem Attn string
 
-crchitab
-	.ds	256
-crclotab
-	.ds	256
+; Lookup tables for CRC-16 calculations
+crchitab	.ds	256
+crclotab	.ds	256
 
 mini2
 
@@ -626,5 +625,11 @@ mini2
 ; Move all of the above crap into banked memory
 
 	.bank
-	*=	$2e2
+	*=	dos_initad
 	.word inittrm
+
+;; This is just a workaround for WUDSN so labels are recognized during development. It is ignored during assembly.
+	.if 0
+	.include icet.asm
+	.endif
+;; End of WUDSN workaround

commit 1279a7d280870bd760828c1ed707bea3cc66551d

Added 'Return sends' to Settings menu.
New submenu 'Return sends' chooses what code is transmitted when the
user presses Return.
In the existing 'Delete sends' submenu added a new option, $7E Backsp
(Atari delete).
Fixed a regression present since 6dece93: parameters were not updated
from the configuration file loaded from disk.

Doc/icet_cfg.h
@@ -11,7 +11,8 @@ struct icet_config_t {
 	uint8_t finescrol = 0; // Enable fine scroll: 0 to disable, 4 to enable. Only one of finescrol or boldallw may be nonzero.
 	uint8_t boldallw = 1; // Enable additional graphics: 0 to disable, 1 for ANSI colors, 2 for bold only, 3 to enable blinking text.
 	uint8_t autowrap = 1; // Wrap around at edge of screen. 1 to enable (normal behavior), 0 to disable.
-	uint8_t delchr = 0; // Code to send when pressing the backspace key. 0 for 0x7f (DEL), 1 for 0x08 (^H, BS)
+	uint8_t delchr = 0; // Bits 0-1: Code to send for Backspace key. 0 - $7f (DEL), 1 - $08 (^H, BS), 2 - $7e (Atari backspace)
+						//  Bits 2-3: Code to send for Return key. 0 for VT100 default, 1 for CR, 2 for LF, 3 for CR+LF.
 	uint8_t bckgrnd = 0; // Screen display mode: 0 for light text on dark background, 1 for reverse.
 	uint8_t bckcolr = 0; // Hue of screen background, 0-15.
 	uint8_t eoltrns = 0; // Downloaded files EOL translation. 0-3 for None/CR/LF/Either. See documentation for details.
@@ -21,7 +22,7 @@ struct icet_config_t {
 	uint8_t eitbit = 1; // Enables PC graphical character set for values 128 and above: 0 to disable, 1 to enable.
 	uint8_t fastr = 2; // Frequency of status calls to serial port device. 0 for normal, 1 for medium, 2 for constant.
 	uint8_t flowctrl = 1; // Flow control method: 0-3 for None, Xon/Xoff, "Rush", Both.
-	uint8_t eolchar = 0; // EOL handling for terminal. 0=CR/LF, 1=LF alone, 2=CR alone, 3=ATASCII (0x9b)
+	uint8_t eolchar = 0; // EOL handling for terminal. 0=CR/LF, 1=LF alone, 2=CR alone, 3=ATASCII ($9b) (3 also accepts ATASCII Tab)
 	uint8_t ascdelay = 2; // In ASCII upload: 0 for no delay between lines, 1-7 for some delay, higher value waits for that character
 		// to arrive from the remote side. Delay values are 1/60 sec, 1/10 sec, 1.5 sec, 1/2 sec, 1 sec, 1.5 sec, 2 sec.
 

src/vt1.asm
@@ -24,17 +24,17 @@ reset				; System reset goes here
 	sta bootflag
 	jsr go_to_dosini	; initialize DOS
 
-	; Restore saved config (reset to same configuration as saved to disk)
-	ldx #cfgnum-1
-?lp
-	lda savddat,x
-	sta cfgdat,x
-	dex
-	bpl ?lp
-
 ; common initialization (either at program load or after reset) continues here
 
 init_continued
+	; Restore saved config (reset to same configuration as saved to disk)
+	ldx #cfgnum-1
+?cfg_lp
+	lda savddat,x
+	sta cfgdat,x
+	dex
+	bpl ?cfg_lp
+
 	; detect R-Time8 cartridge
 	jsr rt8_detect
 	sta rt8_detected

src/vt2.asm
@@ -4367,7 +4367,7 @@ readk
 ; Keypad application mode (numlock off)
 
 keyapp
-	lda #27
+	lda #27		; Esc
 	sta outdat
 	lda #'O		; the letter O
 	sta outdat+1
@@ -4392,7 +4392,7 @@ keyapp
 ?numk1
 	cmp #kretrn
 	bne ?numk5
-	lda #77
+	lda #'M
 	jmp ?numkok
 ?numk5
 	cmp #'q
@@ -4625,39 +4625,55 @@ knocaps
 	jsr getkey
 	jmp shcaps
 knoscaps
-	cmp #kdel
+	cmp #kdel		; Backspace key
 	bne knodel
-	ldx delchr
-	lda deltab,x
+	lda delchr
+	and #$03
+	tax
+	lda deltab,x	; Select code to send for backspace according to user preference
 	sta outdat
 	lda #1
 	sta outnum
-	jmp outputdat
+	bne knodel?ok	; Always branches
 knodel
-	cmp #ksdel
+	cmp #ksdel		; Shift-Backspace
 	bne knosdel
 	lda delchr
-	eor #1
+	and #$03
+	eor #$01		; Choose an alternate backspace character by xoring the index with 1
 	tax
 	lda deltab,x
 	sta outdat
 	lda #1
 	sta outnum
+?ok
 	jmp outputdat
 
-deltab	.byte	127,8
+deltab	.byte	$7f, $08, $7e, $7f
+rettab	.byte	$0d, $0a, $9b
 
 knosdel
-	cmp #kretrn
+	cmp #kretrn		; Return key
 	bne knoret
-kyesret
-	lda #13		; CR
+	lda delchr		; get bits 2-3 of delchr, which determine user preference for Return key
+	lsr a
+	lsr a
+	beq kyesret		; 0 - default behavior
+	tax
+	lda rettab-1,x
+	sta outdat
+	lda #1
+	sta outnum
+	bne kyesret?ok	; always branches
+
+kyesret				; this also handles the numeric keypad Enter, which is not affected by the user config.
+	lda #$0d	; CR
 	sta outdat
 	lda #1
 	sta outnum
 	lda newlmod
 	beq ?ok
-	lda #10		; LF
+	lda #$0a	; LF
 	sta outdat+1
 	inc outnum
 ?ok

src/vt3.asm
@@ -34,8 +34,7 @@ mnmnloop
 	lda lnofbl
 	sta mnlnofbl
 	lda menret
-	cmp #255
-	beq mnquit
+	bmi mnquit
 	cmp #0
 	bne mnnoterm
 	jmp goterm
@@ -57,8 +56,7 @@ mnquit
 	sta mnucnt
 	jsr menudo2
 	lda menret
-	cmp #255
-	beq mnodoquit
+	bmi mnodoquit
 	cmp #0
 	beq mnodoquit
 	pha
@@ -104,19 +102,19 @@ bkopt2
 	lda mnucnt
 	sta svmnucnt
 	lda menret
-	cmp #254	; ****
+	cmp #254	; right arrow key pressed
 	bne ?nr
 	jsr getscrn
 	jsr invrgt
 	jmp settings
 ?nr
-	cmp #253
+	cmp #253	; left arrow key pressed
 	bne ?nl
 	jsr getscrn
 	jsr invlft
 	jmp mnmnloop
 ?nl
-	cmp #255
+	cmp #255	; esc pressed
 	bne ?e
 	jmp mnmenu
 ?e
@@ -151,19 +149,19 @@ bkset2
 	lda mnucnt
 	sta svmnucnt
 	lda menret
-	cmp #254	; ****
+	cmp #254	; right arrow key pressed
 	bne ?nr
 	jsr getscrn
 	jsr invrgt
 	jmp file
 ?nr
-	cmp #253
+	cmp #253	; left arrow key pressed
 	bne ?nl
 	jsr getscrn
 	jsr invlft
 	jmp options
 ?nl
-	cmp #255
+	cmp #255	; esc pressed
 	bne ?e
 	jmp mnmenu
 ?e
@@ -189,8 +187,7 @@ setbps			; Set Baud Rate
 	sta mnucnt
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	lda mnucnt
 	clc
 	adc #8
@@ -212,8 +209,7 @@ setloc			; Set local-echo
 	sta mnucnt
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	sta localecho
 ?n
 	jmp bkset
@@ -228,8 +224,7 @@ seteol			;  Set EOL character(s)
 	sta mnucnt
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	sta eolchar
 ?n
 	jmp bkset
@@ -244,8 +239,7 @@ setfst			;  Set freq. of status calls
 	sta mnucnt
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	sta fastr
 ?n
 	jmp bkset
@@ -260,8 +254,7 @@ setflw			; Set flow control type(s)
 	sta mnucnt
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	sta savflow
 	and #1
 	sta flowctrl
@@ -281,8 +274,7 @@ setbts			; Set no. of Stop bits
 	sta mnucnt
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	clc
 	ror a
 	ror a
@@ -305,8 +297,7 @@ setwrp			;  Set Auto-line-wrap
 	sta mnucnt
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	eor #1
 	sta autowrap
 	sta wrpmode
@@ -347,8 +338,7 @@ setmacros		; Define macros
 	jsr menudo1
 ?done_mn1
 	lda menret
-	cmp #255
-	bne ?ok
+	bpl ?ok
 	jmp bkopt
 ?ok
 
@@ -510,8 +500,7 @@ setclk			; Set Keyclick type
 	sta mnucnt
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	sta click
 ?n
 	jmp bkopt
@@ -527,8 +516,7 @@ setscr			; Special effects department
 	sta mnucnt
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	cmp #0
 	bne ?nz
 	sta finescrol
@@ -581,8 +569,7 @@ setcol			; Set Background colors
 	pla
 	tay
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	pha
 	and #15
 	sta bckcolr
@@ -614,8 +601,7 @@ seteit			; Set PC character set
 	sta mnucnt
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	sta eitbit
 ?n
 	jmp bkopt
@@ -632,16 +618,15 @@ setcrs			;  Set Cursor shape (underscore/block)
 	sta mnucnt
 	jsr menudo1
 	lda menret
+	bmi ?n
 	beq ?c
-	cmp #255
-	beq ?n
 	lda #6
 ?c
 	sta curssiz
 ?n
 	jmp bkopt
 
-setans
+setans			; Emulation menu
 	ldx #>setansw
 	ldy #<setansw
 	jsr drawwin
@@ -651,8 +636,7 @@ setans
 	sta mnucnt
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	sta ansibbs
 	cmp #2
 	bne ?n
@@ -665,6 +649,31 @@ setans
 ?n
 	jmp bkset
 
+setret			; Set Return key
+	ldx #>setretw
+	ldy #<setretw
+	jsr drawwin
+	ldx #>setretd
+	ldy #<setretd
+	lda delchr
+	lsr a
+	lsr a
+	sta mnucnt
+	jsr menudo1
+	lda menret
+	bmi ?n
+	tax
+	lda delchr
+	and #$f3	; reset bits 2-3
+	sta delchr
+	txa
+	asl a
+	asl a
+	ora delchr
+	sta delchr
+?n
+	jmp bkset
+
 setdel			; Set delete key
 	ldx #>setdelw
 	ldy #<setdelw
@@ -672,11 +681,17 @@ setdel			; Set delete key
 	ldx #>setdeld
 	ldy #<setdeld
 	lda delchr
+	and #$3		; just bits 0-1
 	sta mnucnt
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
+	tax
+	lda delchr
+	and #$fc	; reset bits 0-1
+	sta delchr
+	txa
+	ora delchr
 	sta delchr
 ?n
 	jmp bkset
@@ -1270,13 +1285,13 @@ bkxfr2
 	lda #0
 	sta mnplace
 	lda menret
-	cmp #254	; ****
+	cmp #254	; right arrow key pressed
 	bne ?nr
 	jsr getscrn
 	jsr invrgt
 	jmp mnmnloop
 ?nr
-	cmp #253
+	cmp #253	; left arrow key pressed
 	bne ?nl
 	jsr getscrn
 	jsr invlft
@@ -1316,19 +1331,19 @@ bkfil2
 	lda #0
 	sta mnplace
 	lda menret
-	cmp #254	; ****
+	cmp #254	; right arrow key pressed
 	bne ?nr
 	jsr getscrn
 	jsr invrgt
 	jmp xfer
 ?nr
-	cmp #253
+	cmp #253	; left arrow key pressed
 	bne ?nl
 	jsr getscrn
 	jsr invlft
 	jmp settings
 ?nl
-	cmp #255
+	cmp #255	; esc pressed
 	beq filquit
 	lda mnucnt
 	sta svmnucnt
@@ -1625,8 +1640,7 @@ fileol			; Select EOL translation (D/L)
 	ldy #<eoldat
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	lda mnucnt
 	sta eoltrns
 ?n
@@ -1642,8 +1656,7 @@ filuel			; Select EOL translation (U/L)
 	ldy #<ueldat
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	lda mnucnt
 	sta ueltrns
 ?n
@@ -1659,8 +1672,7 @@ filans			; Select ANSI filter
 	ldy #<ansdat
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?n
+	bmi ?n
 	lda mnucnt
 	sta ansiflt
 ?n
@@ -2566,8 +2578,7 @@ ascupl			; Ascii upload
 	ldy #<setasdd
 	jsr menudo1
 	lda menret
-	cmp #255
-	beq ?nn
+	bmi ?nn
 	sta ascdelay
 ?nn
 	jsr getscrn

src/vtdt.asm
@@ -85,27 +85,29 @@ mntbjmp
 	.word	xfer-1
 
 setmnu
-	.byte	30,1,47,12
+	.byte	30,1,47,13
 	.byte	+$80," Baud rate    "
 	.byte	" Local echo   "
 	.byte	" Stop bits    "
 	.byte	" Auto wrap    "
 	.byte	" Emulation    "
+	.byte   " Return sends "
 	.byte	" Delete sends "
 	.byte	" End of line  "
 	.byte	" Status calls "
 	.byte	" Flow control "
 	.byte	" Save config  "
 setmnudta
-	.byte	1,10,14
+	.byte	1,11,14
 	.byte	32,2,32,3,32,4,32,5,32,6,32,7
-	.byte	32,8,32,9,32,10,32,11
+	.byte	32,8,32,9,32,10,32,11,32,12
 settbl
 	.word	setbps-1
 	.word	setloc-1
 	.word	setbts-1
 	.word	setwrp-1
 	.word	setans-1
+	.word	setret-1
 	.word	setdel-1
 	.word	seteol-1
 	.word	setfst-1
@@ -141,13 +143,13 @@ opttbl
 	.word	rsttrm-1
 
 setbpsw
-	.byte	48,1,75,4
+	.byte	44,1,71,4
 	.byte	"  300   600  1200  1800 "
 	.byte	" 2400  4800  9600  19.2 "
 setbpsd
 	.byte	4,2,6
-	.byte	50, 2, 56, 2, 62, 2, 68, 2
-	.byte	50, 3, 56, 3, 62, 3, 68, 3
+	.byte	46, 2, 52, 2, 58, 2, 64, 2
+	.byte	46, 3, 52, 3, 58, 3, 64, 3
 
 setlocw
 	.byte	44,2,53,5
@@ -167,33 +169,33 @@ setansd
 	.byte	46,6,46,7,46,8
 
 seteolw
-	.byte	46,7,59,12
+	.byte	46,8,59,13
 	.byte	" CR + LF  "
 	.byte	" LF alone "
 	.byte	" CR alone "
 	.byte	" ATASCII  "
 seteold
 	.byte	1,4,10
-	.byte	48,8,48,9,48,10,48,11
+	.byte	48,9,48,10,48,11,48,12
 
 setfstw
-	.byte	46,8,59,12
+	.byte	46,9,59,13
 	.byte	" Normal   "
 	.byte	" Medium   "
 	.byte	" Constant "
 setfstd
 	.byte	1,3,10
-	.byte	48,9,48,10,48,11
+	.byte	48,10,48,11,48,12
 
 setflww
-	.byte	46,9,59,14
+	.byte	46,10,59,15
 	.byte	" None     "
 	.byte	" Xon/Xoff "
 	.byte	" ",34,"Rush",34,"   "
 	.byte	" Both     "
 setflwd
 	.byte	1,4,10
-	.byte	48,10,48,11,48,12,48,13
+	.byte	48,11,48,12,48,13,48,14
 
 setbtsw
 	.byte	44,3,55,6
@@ -296,16 +298,27 @@ setcrsd
 	.byte	1,2,8
 	.byte	38,8,38,9
 
+setretw
+	.byte	46,6,59,11
+	.byte	" Default  "
+	.byte	" $0D CR   "
+	.byte	" $0A LF   "
+	.byte	" $9B Ret  "
+setretd
+	.byte	1,4,10
+	.byte	48,7,48,8,48,9,49,10
+
 setdelw
-	.byte	46,6,61,9
+	.byte	46,7,61,11
 	.byte	" $7F DEL    "
 	.byte	" $08 BS ^H  "
+	.byte	" $7E Backsp "
 setdeld
-	.byte	1,2,12
-	.byte	48,7,48,8
+	.byte	1,3,12
+	.byte	48,8,48,9,48,10
 
 savcfgw
-	.byte	46,9,63,12
+	.byte	46,11,63,14
 	.byte	"Saving current"
 	.byte	"  parameters  "
 savcfgwe1

src/vtv.asm
@@ -36,7 +36,7 @@ g1set		.ds 1
 chset		.ds 1
 useset		.ds 1	; in double-width, set to indicate use of Ice-T's character set rather than OS font
 seol		.ds 1	; flag that cursor has written last character in line, so next character will wrap
-newlmod		.ds 1
+newlmod		.ds 1	; VT100 Newline mode
 vt52mode	.ds 1	; VT-52 mode (host controlled)
 numlock		.ds 1
 wrpmode		.ds 1
@@ -48,7 +48,7 @@ boldface	.ds 1	; Bit 0: terminal currently set to write new characters in bold/b
 					; Bit 4: color was set as background.
 gntodo		.ds 1	; When processing Esc '(' or Esc ')' this indicates which one of the two was received.
 qmark		.ds 1	; Some commands start with Esc [ ? - indicate whether we've received the question mark.
-modedo		.ds 1
+modedo		.ds 1	; When handling Esc [ _ h / Esc [ _ l, indicate which of the two we're handling (set/reset).
 ckeysmod	.ds 1
 finnum		.ds 1	; currently parsed decimal number in Esc command sequence
 csi_last_interm	.ds 1	; last 'Intermediate' ($20-2f) character seen in CSI command sequence
@@ -616,7 +616,8 @@ curssiz		.byte 6		; Cursor size: 0 for a block, 6 for underline.
 finescrol	.byte 0		; Enable fine scroll: 0 to disable, 4 to enable. Only one of finescrol or boldallw may be nonzero.
 boldallw	.byte 1		; Enable additional graphics: 0 to disable, 1 for ANSI colors, 2 for bold only, 3 to enable blinking text.
 autowrap	.byte 1		; Wrap around at edge of screen. 1 to enable (normal behavior), 0 to disable.
-delchr		.byte 0		; Code to send when pressing the backspace key. 0 for 0x7f (DEL), 1 for 0x08 (^H, BS)
+delchr		.byte 0		; Bits 0-1: Code to send for Backspace key. 0 - $7f (DEL), 1 - $08 (^H, BS), 2 - $7e (Atari backspace)
+						; Bits 2-3: Code to send for Return key. 0 for VT100 default, 1 for CR, 2 for LF, 3 for $9b (Atari EOL)
 bckgrnd		.byte 0 	; Screen display mode: 0 for light text on dark background, 1 for reverse.
 bckcolr		.byte 0		; Hue of screen background, 0-15.
 eoltrns		.byte 0		; Downloaded files EOL translation. 0-3 for None/CR/LF/Either. See documentation for details.
@@ -626,7 +627,7 @@ ansibbs		.byte 0		; Terminal emulation: 0 for VT-102, 1 for ANSI-BBS, 2 for VT-5
 eitbit		.byte 1		; Enables PC graphical character set for values 128 and above: 0 to disable, 1 to enable.
 fastr		.byte 2		; Frequency of status calls to serial port device. 0 for normal, 1 for medium, 2 for constant.
 flowctrl	.byte 1		; Flow control method: 0-3 for None, Xon/Xoff, "Rush", Both.
-eolchar		.byte 0		; EOL handling for terminal. 0=CR/LF, 1=LF alone, 2=CR alone, 3=ATASCII (0x9b)
+eolchar		.byte 0		; EOL handling for terminal. 0=CR/LF, 1=LF alone, 2=CR alone, 3=ATASCII ($9b) (3 also accepts ATASCII Tab)
 ascdelay	.byte 2		; In ASCII upload: 0 for no delay between lines, 1-7 for some delay, higher value waits for that character
 						; to arrive from the remote side. Delay values are 1/60 sec, 1/10 sec, 1.5 sec, 1/2 sec, 1 sec, 1.5 sec, 2 sec.
 

utils/fontfix.c
@@ -1,18 +1,0 @@
-// fixes order of font from Atari to ASCII. Not used.
-
-#include <stdio.h>
-#include <string.h>
-
-unsigned char chset[1024];
-
-int main() {
-	FILE* fp = fopen("vt_orig.fnt", "rb");
-	memset(chset, 0x1, 1024);
-	fread(chset, 8, 128, fp);
-	fclose(fp);
-	fp = fopen("vt.fnt", "wb");
-	fwrite(chset+64*8, 8, 32, fp);
-	fwrite(chset, 8, 64, fp);
-	fwrite(chset+96*8, 8, 32, fp);
-	fclose(fp);
-}

utils/whitespace_conv.py
@@ -1,15 +1,0 @@
-#!/usr/bin/python
-# 20200429. This was used to fix whitespaces in the asm source files. Probably will never be needed again.
-from __future__ import print_function
-import sys
-
-# file handle fh
-fh = open(sys.argv[1])
-while True:
-	line = fh.readline()
-	if not line:
-		break
-	if len(line) >= 5 and line[0] == '\t' and line[1].isalpha() and line[2].isalpha() and line[3].isalpha() and line[4] == '\t':
-		line = line[:4] + ' ' + line[5:]
-	print(line, end='')
-fh.close()